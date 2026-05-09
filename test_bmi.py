24ec0e594dcbb4b7cf1aed2087c3afed10bf8613
vishal jn <vishaljn.25@ichat.sp.edu.sg>
2026-05-09 19:06:34 +0800

    THis is testing for bmi.py

--- test_bmi.py
+++ test_bmi.py
@@ -2,21 +2,18 @@ import Lab2.bmi as bmi
 
 
 def test_bmi_normal_weight():
+  y = bmi.calculate_bmi(70,1.74)
+  assert y == 0
   print("You are in normal weight classification")
+  
 def test_bmi_over_weight():
+  y = bmi.calculate_bmi(80, 1.74)
+  assert y == 1
   print("You are in overweight classification")
 def test_bmi_under_weight():
+  y = bmi.calculate_bmi(40, 1.74)
+  assert y == -1
   print("You are in underweight classification")
  
-
-y = bmi.calculate_bmi()
-if (y == -1):
-  test_bmi_under_weight()
-elif y == 0 :
- test_bmi_normal_weight()
-else:
- test_bmi_over_weight()
-
-
 
 
